9ade36fb4fbecadba55fd5a259a6396ec5293b86
Rijalinor <firediamond552255@gmail.com>
2026-02-28 15:46:31 +0800

    fix: update TextToSpeech import to com.getcapacitor.community.tts

--- android/app/src/main/java/com/deliveryapp/driver/MainActivity.java
+++ android/app/src/main/java/com/deliveryapp/driver/MainActivity.java
@@ -2,7 +2,7 @@ package com.deliveryapp.driver;
 
 import com.getcapacitor.BridgeActivity;
 import android.os.Bundle;
-import com.capacitorcommunity.texttospeech.TextToSpeech;
+import com.getcapacitor.community.tts.TextToSpeech;
 
 public class MainActivity extends BridgeActivity {
     @Override
