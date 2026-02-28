b26edacb916be3f006b9741f8abc8b0b0dcc87c0
Rijalinor <firediamond552255@gmail.com>
2026-02-28 15:40:53 +0800

    fix: manually register TextToSpeech plugin in MainActivity

--- android/app/src/main/java/com/deliveryapp/driver/MainActivity.java
+++ android/app/src/main/java/com/deliveryapp/driver/MainActivity.java
@@ -1,5 +1,13 @@
 package com.deliveryapp.driver;
 
 import com.getcapacitor.BridgeActivity;
+import android.os.Bundle;
+import com.capacitorcommunity.texttospeech.TextToSpeech;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        registerPlugin(TextToSpeech.class);
+    }
+}
